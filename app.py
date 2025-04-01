af113c9ec41fa26ef1d079db0e92424b3973bd9b
RafaelRodrigues17 <rrodriguesmattos27@gmail.com>
2025-04-01 16:14:48 -0300

    Atualização do código

--- app.py
+++ app.py
@@ -3,15 +3,22 @@ from flask import Flask, render_template, request # Importando a biblioteca Flas
 from werkzeug.security import generate_password_hash, check_password_hash
 import database
 
-app = Flask(__name__) # Criando um objeto do Flask chamado 
+app = Flask(__name__) # Criando um objeto do Flask
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
+    if request.method == "POST":
+        form = request.form
+        if database.login(form) == True:
+            return render_template('lista.html')
+        else:
+            return "Ocorreu um erro ao fazer o login do usuário"     
+    else:    
+       return render_template('login.html')
 
 @app.route('/cadastro', methods=["GET", "POST"])
 def cadastro():
--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def conectar_banco():
     conexao = sqlite3.connect("tarefas.db")
@@ -25,14 +26,34 @@ def criar_usuario(formulario):
     quantidade_de_emails = cursor.fetchone()
     print(quantidade_de_emails)
     if (quantidade_de_emails [0]> 0):
-        print("E-mail já cadastrado! escolha outro")
+        print("E-mail já cadastrado! tente novamente")
         return False
-
+    
+    senha_criptografada = generate_password_hash(formulario['senha'])
     cursor.execute('''insert into usuarios(email, nome, senha) 
-                   VALUES (?, ?, ?)''', (formulario['email'], formulario['nome'], formulario['senha']
-                       ))
+                   VALUES (?, ?, ?)''', (formulario['email'], formulario['nome'], senha_criptografada))
+    
     conexao.commit()
     return True
+
+def login(formulario):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''SELECT COUNT (email) FROM usuarios WHERE email=?''', (formulario['email'],))
+    conexao.commit()
     
+    quantidade_de_emails = cursor.fetchone()
+    print(quantidade_de_emails)
+    if (quantidade_de_emails [0]== 0):
+        print("E-mail não cadastrado! tente novamente")
+        return False
+    
+    cursor.execute('''select senha FROM usuarios WHERE
+                   email=? ''', (formulario['email'],))
+    
+    conexao.commit()
+    senha_criptografada = cursor.fetchone()
+    return check_password_hash (senha_criptografada [0], formulario['senha'])
+
 if __name__ == "__main__":
     criar_tabelas()
